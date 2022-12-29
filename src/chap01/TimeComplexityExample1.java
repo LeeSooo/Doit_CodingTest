a46c766ac9e3d0005a8104c7e55ce515f7d1d3df
LeeSooo <dltn990930@naver.com>
2022-12-29 16:21:01 +0900

    chap01.코딩 테스트 준비하기 (2022-12-29 이수)

--- src/chap01/TimeComplexityExample1.java
+++ src/chap01/TimeComplexityExample1.java
@@ -1,11 +1,5 @@
 package chap01;
 
-/* < 시간 복잡도 정의 >
- * 빅-오메가 : 최선일 때의 연산 횟수를 나타낸 표기법
- * 빅-세타 : 보통일 때의 연산 횟수를 나타낸 표기법
- * 빅-오 : 최악일 때의 연산 횟수를 나타낸 표기법 
- */
-
 /* < 시간 복잡도 예제 코드 >
  * 빅-오메가 표기법 : 1번
  * 빅-세타 표기법 : 2/N번
@@ -14,7 +8,7 @@ package chap01;
 
 // 코딩테스트는 "빅-오 표기법"을 기준으로 수행시간을 계산하는 것이 좋다.
 
-public class Ex01_TimeComplexityExample1 {
+public class TimeComplexityExample1 {
 	public static void main(String[] args) {
 		// 1~100 사이의 램덤 선택
 		int findNumber = (int)(Math.random() * 100);

45011683d11905ddaa9807375e88492f3ed50752
LeeSooo <dltn990930@naver.com>
2022-12-31 21:10:55 +0900

    chap03.자료구조(2022-12-31 이수)

---
+++ src/chap03_DataStructure/Q002_P1546.java
@@ -0,0 +1,46 @@
+package chap03_DataStructure;
+
+/* 002번 문제 : 새로운 평균(성적 / 최고점수 * 100) 을 구해서 출력하시오.
+ * 입력 : 1번째 줄(시험을 본 과목의 개수 n), 2번째 줄(모든과목의 성적)
+ * 출력 : 새로운 평균(성적 / 최고점수 * 100)을 출력.(실제 정답과 출력값의 절대 오차 또는 상대 오차가 0.01 이하이면 정답.)
+ */
+
+/* <슈도코드>
+ *  (1) 1번째 줄 - 시험을 본 과목의 개수 n을 입력받기.
+ *  (2) 2번째 줄 - 시험 성적을 순서대로 입력받기.
+ *  (3)	최고점수와 총 시험성적을 구하기.
+ *  (4) 새로운 평균을 구하여 출력하기.
+ */
+
+import java.util.Scanner;
+
+public class Q002_P1546 {
+	public static void main(String[] args) {
+		// (1) 1번째 줄 - 시험을 본 과목의 개수 n을 입력받기.
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		// (2) 2번째 줄 - 시험 성적을 순서대로 입력받기.
+		// (3) 최고점수와 총 합계 구하기.
+		int[] arr = new int[n];
+		
+		// ** 자료형 범위 오류를 방지하기 위해서 long형 선언 **
+		long sum = 0;
+		long max = 0;
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+			
+			// 최고 점수 구하기.
+			if (max < arr[i])
+				max = arr[i];
+			
+			// 합계 구하기.
+			sum += arr[i];
+		}
+		sc.close();
+		
+		// (4) 새로운 평균을 구하여 출력하기. (100.0 => 출력결과가 소수로 나오게 하기 위함.)
+		System.out.println(sum * 100.0 / max / n);
+	}
+}
---
+++ src/chap03_DataStructure/Q003_P11659.java
@@ -0,0 +1,43 @@
+package chap03_DataStructure;
+
+/* 003번 문제 : 수 N개가 주어졌을 때, i번째 수 에서 j번째 수까지의 합을 구하는 프로그램을 작성하시오.
+ * 입력 : 1번째 줄(제공되는 숫자의 개수 n)(구간 합을 구해야 하는 줄의 개수 m), 2번째 줄(제공되는 숫자), 그 이후(구간 합을 구할 자리수 2개)
+ * 출력 : 1번째 줄(합계), 그 이후(구간 합)
+ */
+
+/* <슈도코드>
+ *  (1) 1번째 줄 - 제공되는 숫자의 개수 n, 구간 합을 구해야 하는 줄의 개수 m 입력받기
+ *  (2) 2번째 줄 - 합배열 구하기. (s[i] = s[i-1] + a[i])
+ *  (3) 그 이후 줄 - 구간합 구하고 출력하기. (s[j] - s[i]) 구간합 최대 자리 수 - 구간합 최소 자리 수
+ */
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q003_P11659 {
+	public static void main(String[] args) throws IOException {
+		// (1) 1번째 줄 - 제공되는 숫자의 개수 n, 구간 합을 구해야 하는 줄의 개수 m 입력받기
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		// (2) 2번째 줄 - 합배열 구하기. (출력 X)
+		long[] s = new long[n+1];
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i = 1; i < s.length; i++) {	// (i의 조건 조심하기!)
+			s[i] = s[i-1] + Integer.parseInt(st.nextToken());
+		}
+		
+		// (3) 그 이후 줄 - 구간합 구하고 출력하기.
+		for(int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int min = Integer.parseInt(st.nextToken());
+			int max = Integer.parseInt(st.nextToken());
+			System.out.println(s[max] - s[min-1]);
+		}
+	}
+}

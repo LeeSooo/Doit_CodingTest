5e3d02825bbb84baac0c4f73ccd1595548652ab7
LeeSooo <dltn990930@naver.com>
2023-01-01 20:07:04 +0900

    chap03.자료구조(2023-01-01 이수)

---
+++ src/chap03_DataStructure/Q004_P11660.java
@@ -0,0 +1,25 @@
+package chap03_DataStructure;
+
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q004_P11660 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[][] a = new int[n+1][n+1];
+		
+		for(int i = 1; i<=n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 1; j <= n; j++) {
+			}
+		}
+		
+	}
+
+}
